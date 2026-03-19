51401c8bb1f1b70cad2159125634cea8e595d643
Ankit Sharma <116503887+Ankitshrma25@users.noreply.github.com>
2026-03-19 13:22:21 +0530

    Data Transformation Component Done

--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -25,3 +25,32 @@ def write_yaml_file(file_path: str, content: object, replace: bool = False) -> N
             yaml.dump(content, file)
     except Exception as e:
         raise NetworkSecurityException(e, sys)
+    
+
+# function to save numpy array data to file
+def save_numpy_array_data(file_path: str, array: np.array):
+    """
+    Save numpy array data to file
+    file_path: str location of the file to save
+    array: np.array data to save
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            np.save(file_obj, array)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys) from e
+    
+
+
+# function to save pickle file
+def save_object(file_path: str, obj: object) -> None:
+    try:
+        logging.info(f"Saving object to file: {file_path}")
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+            logging.info(f"Object saved successfully to file: {file_path}")
+    except Exception as e:
+        raise NetworkSecurityException(e, sys) from e
